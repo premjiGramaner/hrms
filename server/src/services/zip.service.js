ccc5b6c21be1d117c425862acb2b939b0c0b9e44
prashanth <prashanth.v@cannyfore.com>
2026-07-06 18:20:51 +0530

    Formated code

--- server/src/services/zip.service.js
+++ server/src/services/zip.service.js
@@ -9,7 +9,7 @@ const crcTable = new Uint32Array(256).map((_, index) => {
 function crc32(buffer) {
   let crc = 0xffffffff;
   for (const byte of buffer) {
-    crc = crcTable[(crc ^ byte) & 0xff] ^ (crc >>> 8);
+    crc = crcTable[(crc ^ byte) & 0xff] ^ (crc >>> 8); 
   }
   return (crc ^ 0xffffffff) >>> 0;
 }
