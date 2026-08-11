fccae267c9628143e21344003c8c20e65b53deeb
DineshNallaiya <dinesh.nallaiya@cannyfore.com>
2026-08-11 21:12:55 +0530

    Feat: Added config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,13 +5,27 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    allowedHosts: true,
+    //  proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:5001',
+    //     changeOrigin: true,
+    //   },
+    //   '/uploads': {
+    //     target: 'http://localhost:5001',
+    //     changeOrigin: true,
+    //   },
+    // }
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://f79f-2402-3a80-1821-96ab-ad27-3ae2-7962-7bec.ngrok-free.app',
         changeOrigin: true,
+        headers: {
+      'ngrok-skip-browser-warning': 'true',
+    },
       },
       '/uploads': {
-        target: 'http://localhost:5001',
+        target: 'https://f79f-2402-3a80-1821-96ab-ad27-3ae2-7962-7bec.ngrok-free.app',
         changeOrigin: true,
       },
     },
